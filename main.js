3b9ae61066276e3308c197bf78829567e85a94c0
Shumimran <164326671+Shumimran@users.noreply.github.com>
2024-06-05 11:28:23 +0500

    Add files via upload
    Author: Shumaila Imran

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+// Making a Student Template: Create a blueprint for student information, including their
+//  name, age, and the classes they're taking, and then fill in this blueprint with an
+//   example student.
+;
+let student = {
+    name: "shumaila",
+    age: 42,
+    courses: ["Maths", "computer", "Science"]
+};
+console.log(student);
